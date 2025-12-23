f24d002d52023f04df97b96b4e5d6e1a47e9db58
Isaiah Calvo <isaiahcalvo123@gmail.com>
2025-12-23 18:21:11 -0500

    Backup: 2025-12-23 18:21:10

--- enable-user-settings.js
+++
@@ -1,6 +0,0 @@
-// Run this in your browser console AFTER creating the user_settings table
-// This will re-enable the user_settings feature
-
-localStorage.setItem('userSettingsTableUnavailable', 'false');
-console.log('✅ User settings re-enabled! Reload the page to activate.');
-location.reload();
